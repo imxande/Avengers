651eb0f67ce8210afbf4cce8765b3cffd7c77a88
imxande <alerodriguezlarosa@gmail.com>
2020-07-20 04:14:45 -0400

    created nav with link in App componnent

--- avengers/src/App.js
+++ avengers/src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./App.css";
 import Home from "./components/Home";
+import { Route, Link, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div>
+      <nav className="nav">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+      </nav>
       <Home />
     </div>
   );
