3e72f03b80980b98e6f481f2e9f3a18dbbd4d4fc
imxande <alerodriguezlarosa@gmail.com>
2020-07-20 04:04:21 -0400

    cleaned up APP componnet, Home componnent renders

--- avengers/src/App.js
+++ avengers/src/App.js
@@ -1,15 +1,11 @@
 import React from "react";
 import "./App.css";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="home">
-      <h1>Home</h1>
-      <img
-        className="home-img"
-        src="https://cdn1.thr.com/sites/default/files/2018/11/avengers_earths_mightiest_art_0.jpg"
-        alt="Avengers comic book"
-      />
+    <div>
+      <Home />
     </div>
   );
 }
