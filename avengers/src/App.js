2804e670bc8254b196635c977fcd453bf86dfa3e
imxande <alerodriguezlarosa@gmail.com>
2020-07-20 04:01:06 -0400

    added avengers image to the home component, and styled

--- avengers/src/App.js
+++ avengers/src/App.js
@@ -2,7 +2,16 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="home">
+      <h1>Home</h1>
+      <img
+        className="home-img"
+        src="https://cdn1.thr.com/sites/default/files/2018/11/avengers_earths_mightiest_art_0.jpg"
+        alt="Avengers comic book"
+      />
+    </div>
+  );
 }
 
 export default App;
