03583017f62cdd7316eb0e7a25a4decaffa7b100
imxande <alerodriguezlarosa@gmail.com>
2020-07-20 04:55:49 -0400

    Avenger component renders

---
+++ avengers/src/components/Avenger.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Avenger = (props) => {
+  console.log("Avenger:", props);
+  return <div>I am Are you?</div>;
+};
+
+export default Avenger;
--- avengers/src/components/AvengersList.js
+++ avengers/src/components/AvengersList.js
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
 import avengersData from "../data/data";
+import Avenger from "./Avenger";
 
 const AvengersList = () => {
   // data state
   const [data, setData] = useState(avengersData);
-  console.log(data);
+  console.log("Data: ", data);
 
   return (
     <div className="avengers">
       <h1>List of Avengers</h1>
+      {data.map((avenger, index) => {
+        return <Avenger key={index} avenger={avenger} />;
+      })}
     </div>
   );
 };
